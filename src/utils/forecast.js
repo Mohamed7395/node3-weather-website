51913e1fe3c1050e003276ed700e6fbc7f8be270
SparKs <mohammed.elkholy.1995@gmail.com>
2019-11-02 03:00:01 +0200

    init commint

---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require ("request");
+
+const forecast = (latitude, longitude, callback) => {
+    const url = "https://api.darksky.net/forecast/8217029cb0b94d15a901b16bed1a03b2/" + encodeURIComponent(latitude) + "," + encodeURIComponent(longitude) + "?units=si"; 
+    
+    request({ url, json: true }, (error, {body}) => {
+        if (error){
+            callback("Unable to connect to weather service!", undefined)
+        } else if (body.error) {
+            callback("Unable to find location!", undefined)
+        } else {
+            const summary = body.daily.data[0].summary;
+            const temperature = body.currently.temperature;
+            const rain = body.currently.precipProbability;
+            callback(undefined, summary + " It's currently " + temperature + " degrees out. There is a " + rain + "% chance of rain.");
+        }
+    })
+}
+
+module.exports = forecast;
---
+++ src/utils/geocode.js
@@ -0,0 +1,21 @@
+const request = require("request");
+
+const geocode = (address, callback) => {
+    const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + address + ".json?access_token=pk.eyJ1IjoibW9oYW1lZHNhbWVlciIsImEiOiJjazFyMmZtNHMwMHVrM2NuMXo3Ynd3N2Y0In0.TFQiKWxkOCV_VGcdjTZ2NA&limit=1"
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback("Unable to connect to location services!", undefined);
+        } else if (body.features.length === 0) {
+            callback("Unable to find location, try another search !", undefined);
+        } else {
+            callback(undefined, { 
+                location: body.features[0].place_name,
+                longitude: body.features[0].center[0],
+                latitude: body.features[0].center[1]
+            })
+        }
+    }) 
+}
+
+module.exports = geocode;
